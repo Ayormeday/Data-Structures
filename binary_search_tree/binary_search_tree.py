21395ca5c643774c532374ba52ff66b0f4f2f243
Ayomide Jones <ayormeday@gmail.com>
2020-02-27 08:38:16 +0100

    insert function created and tested

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,8 +12,19 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left: #if left value doesn't exist
+                self.left = BinarySearchTree(value) # make a new node
+            else: 
+                self.left.insert(value) # if left node exist, recurse
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
+    
+    
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
